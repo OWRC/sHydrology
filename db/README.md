4e2c46b7e7d51b6e5a6de0e030bb200e76449ef3
maseology <mason@riffle.ca>
2018-01-19 14:14:09 -0500

    update to version 1.0, map only

--- db/README.md
+++
@@ -1,7 +0,0 @@
-# sHydrology
-
-As coded, the sHydrology.R Shiny-Leaflet application is built to view the Water Survey of Canada (WSC) **HY**drological **DAT**abase (HYDAT) which can be downloaded [here](https://www.ec.gc.ca/rhc-wsc/default.asp?lang=En&n=9018B5EC-1). sHydrology.R is defaulted to Ontario gauges only.
-
-Download and extract the SQLite format of the database typically compressed as *'Hydat_sqlite3_YYYYMMDD.zip'*, where *'YYYYMMDD'* is the date of release. Extract the SQLite file *'Hydat.sqlite3'* and place in the */db/* directory.
-
-*'Hydat.sqlite3'* is roughly 1GB in size and thus cannot be hosted on GitHub.
--- functions/HYDAT_query.R
+++ functions/HYDAT_query.R
@@ -3,18 +3,17 @@
 ##########################################################
 # By M. Marchildon
 #
-# Aug 21, 2017
+# Jan 19, 2018
 ##########################################################
 
 library(RSQLite)
-library(zoo)
 
 
 ###########################################################################################
 ## connect to the HYDAT sqlite3 file
 ###########################################################################################
-dbConn <- function(dbName){
-  dbc <- dbConnect(RSQLite::SQLite(), dbname=dbName)
+dbcnxn <- function(dbFP){
+  dbc <- dbConnect(RSQLite::SQLite(), dbname=dbFP)
   # get a list of all tables
   # dbListTables(dbc)  
   return(dbc)
@@ -24,11 +23,11 @@ dbConn <- function(dbName){
 ###########################################################################################
 ## collect locations
 ###########################################################################################
-StaLocations <- function(dbc, prov = "ON"){
+qStaLoc <- function(dbc, prov=NULL){
 
   # get the STATIONS table as a data.frame
   tblSta <- dbGetQuery(dbc,'select * from STATIONS')
-  tblSta <- tblSta[tblSta$PROV_TERR_STATE_LOC == "ON",]
+  if (!is.null(prov)){tblSta <- tblSta[tblSta$PROV_TERR_STATE_LOC == prov,]}
   tblSta <- tblSta[!is.na(tblSta$DRAINAGE_AREA_GROSS),]
   tblSta <- tblSta[!is.na(tblSta$LATITUDE),]
   
@@ -39,14 +38,22 @@ StaLocations <- function(dbc, prov = "ON"){
   for (s in tblSta$STATION_NUMBER){
     q <- dbGetQuery(dbc, paste0('select * from DLY_FLOWS where STATION_NUMBER = "',s,'"'))
     cnt <- cnt + 1
-    YRb[cnt] <- min(q$YEAR)
-    YRe[cnt] <- max(q$YEAR)
+    if (nrow(q)==0) {
+      # print(paste0('no data for station ',s))
+      YRb[cnt] <- NA
+      YRe[cnt] <- NA
+    } else {
+      YRb[cnt] <- min(q$YEAR)
+      YRe[cnt] <- max(q$YEAR)      
+    }
   }
   tblSta$StartYear <- YRb
   tblSta$EndYear <- YRe
-  tblSta <- tblSta[!is.infinite(tblSta$StartYear),]
+  tblSta$sID <- tblSta$STATION_NUMBER
   tblSta <- tblSta[!is.na(tblSta$StartYear),]
   tblSta <- tblSta[!is.na(tblSta$EndYear),]
+  colnames(tblSta)[1] <- "sName"
+  colnames(tblSta)[2] <- "sName2"
   return(tblSta)
 }
 
@@ -69,6 +76,7 @@ qStaCarea <- function(dbc,staID){
 ###########################################################################################
 qTemporal <- function(dbc,staID){
   qFlow <- dbGetQuery(dbc, paste0('select * from DLY_FLOWS where STATION_NUMBER = "',staID,'"'))
+  # qFlow <- dbGetQuery(dbc, 'select * from DLY_FLOWS where STATION_NUMBER = "02HB002"')
   DTb <- as.Date(paste0(as.numeric(qFlow[1,2]),'-',as.numeric(qFlow[1,3]),'-01'))
   DTe <- as.Date(paste0(as.numeric(tail(qFlow[2],1)),'-',as.numeric(tail(qFlow[3],1)),'-01'))
   POR <- as.numeric(DTe-DTb)
@@ -93,5 +101,45 @@ qTemporal <- function(dbc,staID){
   # anyDuplicated(Date)
   Flag[is.na(Flag)] <- ""
   hyd <- data.frame(Date,Flow,Flag)
+  # dfQ <- hyd[!is.na(hyd$Date),]
   return(hyd[!is.na(hyd$Date),])
+}
+
+
+###########################################################################################
+## HYDAT temporal Query (of many stations)
+###########################################################################################
+qTemporal.many <- function(dbc,stalst){
+  stStations <- paste0( 'STATION_NUMBER = "',stalst[1],'"')
+  for (i in 2:length(stalst)) {
+    stStations <- paste0(stStations, ' OR STATION_NUMBER = "', stalst[i], '"')
+  }
+  qFlow <- dbGetQuery(dbc, paste0('select * from DLY_FLOWS where ', stStations))
+  Station <- vector('character', length=nrow(qFlow))
+  Flow <- vector('numeric', length=nrow(qFlow))
+  Flag <- vector('character', length=nrow(qFlow))
+  Date <- vector('character', length=nrow(qFlow))
+  cnt <- 0
+  
+  for(i in 1:nrow(qFlow)){
+    sta <- qFlow[i,1]
+    yr <- qFlow[i,2]
+    mo <- as.numeric(qFlow[i,3])
+    
+    for(d in 1:qFlow[i,5]){
+      cnt <- cnt + 1
+      Date[cnt] <- paste0(yr,'-',mo,'-',d)
+      Station[cnt] <- sta
+      Flow[cnt] <- qFlow[i,(d-1)*2+12]
+      Flag[cnt] <- qFlow[i,(d-1)*2+13]
+    }
+  }
+  
+  Date <- as.Date(Date)
+  # anyDuplicated(Date)
+  Flag[is.na(Flag)] <- ""
+  hyd <- data.frame(Station,Date,Flow,Flag)
+  hyd <- hyd[!is.na(hyd$Date),]
+  hyd <- hyd[!is.na(hyd$Flow),]
+  return(hyd)
 }
