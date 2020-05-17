696c0af90b695afeedf4e26a27a6e952edf24779
Mason Marchildon <mason@riffle.ca>
2020-05-17 15:54:55 -0400

    1.5.0

--- pkg/mobile_srv.R
+++
@@ -1,5 +0,0 @@
-
-################################
-## modify app for mobile devices
-## modified from: https://g3rv4.com/2017/08/shiny-detect-mobile-browsers
-observe({ if (input$isMobile) { toggle('panl') } })
