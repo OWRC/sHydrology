ed2dff10d8e2cda888bd1ae1b28d7c43549e5f9c
maseology <mason@riffle.ca>
2020-05-17 15:33:52 -0400

    1.5.0

--- app.R
+++ app.R
@@ -20,7 +20,6 @@ shinyApp(
     useShinyjs(),
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
     tags$head(includeCSS("pkg/styles.css")),
-    # tags$head(includeScript("pkg/mobile.js")),
     
     div(
       id = "app-content",
