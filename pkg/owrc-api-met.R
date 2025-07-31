2f6d805bbc97990206ba1c7ede37d89e415297fa
maseology <maseology@pm.me>
2025-07-31 08:27:44 -0400

    bug fix

--- pkg/owrc-api-met.R
+++ pkg/owrc-api-met.R
@@ -1,10 +1,27 @@
 
+getCDS <- function(url){
+  out <- tryCatch(
+    {
+      jsonlite::fromJSON(url)
+    },
+    error=function(cond) {
+      return(NULL)
+    },
+    warning=function(cond) {
+      return(NULL)
+    },
+    finally={}
+  )
+  out[out==-999] <- NA
+  return(out)
+}
 
 getMeteo <- function(lat,lng) {
   # collect interpolated data
   url <- paste0('http://fews.oakridgeswater.ca:8080/dymetc/',lat,'/',lng)
   print(url)
-  df <- jsonlite::fromJSON(url)
+  df <- getCDS(url)
+  if ( length(df)==0) return(NULL)
   if ( length(df)==1 && df=="NA" ) return(NULL)
   df[df == -999] <- NA # do this before converting date
   df$Date = as.Date(df$Date)
